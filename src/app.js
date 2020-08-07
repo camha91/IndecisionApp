8d166c66414c951340bc7c1074b79c4d863109a5
camha91 <crystalhanguyen@gmail.com>
2020-08-06 21:30:32 -0700

    One Component per File

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from "./components/IndecisionApp";
 
-const template = <p>THIS IS JSX FROM WEBPACK</p>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
