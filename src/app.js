9854b66f5fce85f01c19690da9a5ddfb7f1c0350
camha91 <crystalhanguyen@gmail.com>
2020-08-10 15:51:00 -0700

    Setting up Webpack with SCSS

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/IndecisionApp";
+import './styles/styles.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
